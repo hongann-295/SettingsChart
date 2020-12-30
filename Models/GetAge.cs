10067a24c06d2deaf1343853bffe08586aa4fc96
an hong <hongan529@gmail.com>
2020-12-30 15:56:02 +0700

    test

---
+++ Models/GetAge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Christoc.Modules.SettingsChart2.Models
+{
+    public class GetAge
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Models/Help.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Christoc.Modules.SettingsChart2.Models
+{
+    static class Help
+    {
+       public static List<int> GetX(string dadas, List<Models.GetPerson> peoples)
+        {
+           /* var people = new List<object>();*/
+
+            if (dadas == "Id")
+            {
+                return peoples.Select(p =>  p.Id).ToList();
+            }
+            return null;
+        }
+
+
+    }
+}
